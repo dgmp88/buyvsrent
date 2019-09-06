43f7940b2fc8fd6cdbd892604aac5c2e6bb5d415
George Prichard <dgmprichard@gmail.com>
2019-09-06 09:36:43 +0100

    small corrections

--- buy_vs_rent.py
+++ buy_vs_rent.py
@@ -1,96 +1,132 @@
 #!/usr/bin/env python3
 years = 25
-initial_investment = 150000
-monthly_total = 1800
+initial_investment = 100000
+monthly_total = 2500
 rent_payments = 1250
 mortgage_payments = 1250
-house_price = 400000
+house_value = 400000
 
 savings_interest = 0.07
 mortgage_interest = 0.03
 house_growth = 0.03
 
 
-def calc_compound_interest(
-    start_sum, interest_rate, years, step_add=0, compounding_steps=12
+def calc_net_assets(
+    years,
+    savings_start,
+    savings_interest,
+    savings_payments,
+    mortgage_start=0,
+    mortgage_interest=0,
+    mortgage_payments=0,
+    house_value=0,
+    house_yearly_growth=0,
 ):
-    total = start_sum
-    total_added = start_sum
-    for _ in range(years):
-        for _ in range(compounding_steps):
-            # Add interest
-            interest = total * (interest_rate / compounding_steps)
-            total += interest
 
-            # Add extra investment
-            total += step_add
-            total_added += step_add
-    return total, total_added
+    info = dict(
+        savings_start=savings_start,
+        savings_payments=savings_payments,
+        mortgage_start=mortgage_start,
+        mortgage_payments=mortgage_payments,
+        mortgage=mortgage_start,
+        mortgage_paid=house_value - mortgage_start,
+        savings=savings_start,
+        savings_paid=savings_start,
+        house_value=house_value,
+    )
+    one_off_savings = 0
+
+    for year in range(years):
+        # Compound monthly
+        for month in range(12):
+
+            ## Calculate mortgage value
+            m_interest = info["mortgage"] * (mortgage_interest / 12)
+            info["mortgage"] += m_interest
+
+            # Subtract payments
+            info["mortgage"] -= mortgage_payments
+            info["mortgage_paid"] += mortgage_payments
+
+            # The month the mortgage is paid off, put everything into savings
+            if info["mortgage"] <= 0 and mortgage_payments != 0:
+                one_off_savings = -info["mortgage"]
+                savings_payments += mortgage_payments
+                mortgage_payments = 0
+                info["mortgage"] = 0
+                info["mortage paid off year"] = year
+
+            ## Calculate savings value
+            # Add savings interest
+            s_interest = info["savings"] * (savings_interest / 12)
+            info["savings"] += s_interest
+
+            # Add savings
+            info["savings"] += savings_payments + one_off_savings
+            info["savings_paid"] += savings_payments + one_off_savings
+            one_off_savings = 0
+
+        # Update house price (annually, it doesn't compound)
+        house_value_accrued = info["house_value"] * house_growth
+        info["house_value"] += house_value_accrued
+
+    info["total_assets"] = 0
+
+    if info["mortgage_paid"] > 0:
+        house_fraction_owned = 1 - (
+            info["mortgage"] / (info["mortgage"] + info["mortgage_paid"])
+        )
+        info["house_percent_owned"] = house_fraction_owned * 100
+
+        info["total_assets"] += house_fraction_owned * info["house_value"]
+
+    info["total_assets"] += info["savings"] - info["mortgage"]
+    return info
 
 
-def print_row(name, val):
-    val = round(val)
-    text = f"{name: <50}{val:,}"
-    print(text)
+def print_info(info):
+    print("*" * 50)
+    for k, v in info.items():
+        v = round(v)
+        text = f"{k: <50}{v:,}"
+        print(text)
 
 
 ## Calculate the savings total
-monthly_savings = monthly_total - rent_payments
-
-savings_total, total_saved = calc_compound_interest(
-    initial_investment, savings_interest, years, step_add=monthly_savings
+info = calc_net_assets(
+    years, initial_investment, savings_interest, monthly_total - rent_payments
 )
-
-
-print_row("Total value of savings: ", savings_total)
-print_row("Total value of assets: ", savings_total)
-print_row("Total saved: ", total_saved)
+print_info(info)
 
 ## Calculate the house buying + savings total
 
-mortgage = house_price - initial_investment
-total_mortgage_paid = initial_investment
+info = calc_net_assets(
+    years,
+    0,
+    savings_interest,
+    monthly_total - mortgage_payments,
+    house_value - initial_investment,
+    mortgage_interest,
+    mortgage_payments,
+    house_value,
+    house_growth,
+)
 
-monthly_savings = monthly_total - mortgage_payments
-savings_total = 0
-savings_added = 0
-one_off_add = 0
-current_house_price = house_price
+print_info(info)
 
 
-for _ in range(years):
-    for _ in range(12):
-        # Add interest
-        interest = mortgage * (mortgage_interest / 12)
-        mortgage += interest
+## Less in mortgage
+house_deposit = 50000
+info = calc_net_assets(
+    years,
+    initial_investment - house_deposit,
+    savings_interest,
+    monthly_total - mortgage_payments,
+    house_value - house_deposit,
+    mortgage_interest,
+    mortgage_payments,
+    house_value,
+    house_growth,
+)
 
-        # Subtract payments
-        mortgage -= mortgage_payments
-        total_mortgage_paid += mortgage_payments
-
-        # The month the mortgage is paid off, put everything int savings
-        if mortgage < 0:
-            one_off_add = -mortgage
-            mortgage_payments = 0
-            mortgage = 0
-            monthly_savings = monthly_total
-
-        # Add savings interest
-        interest = savings_total * (savings_interest / 12)
-        savings_total += interest
-
-        # Add savings
-        savings_total += monthly_savings + one_off_add
-        savings_added += monthly_savings + one_off_add
-
-        one_off_add = 0
-
-        # Update house price
-        house_value_accrued = current_house_price * (house_growth / 12)
-        current_house_price += house_value_accrued
-
-print("*" * 50)
-print_row("Total value of savings: ", savings_total)
-print_row("Total value of house: ", current_house_price)
-print_row("Total value of assets: ", current_house_price + savings_total)
-print_row("Total saved: ", total_saved)
+print_info(info)
