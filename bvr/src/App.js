1c26769483f63bd046d7dd76f0491b9c1412eccf
CPrich905 <charlie.prichard@gmail.com>
2019-09-17 14:28:55 +0100

    BVR commit 1

--- bvr/src/App.js
+++ bvr/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserInputs from './components/UserInputs';
 
 function App() {
   return (
@@ -9,9 +10,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Buy vs Rent
+
         </p>
-    
+
       </header>
+
+      <main>
+        <UserInputs />
+      </main>
     </div>
   );
 }
---
+++ bvr/src/components/UserInputs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class UserInputs extends React.Component {
+  constructor() {
+    super ()
+    this.state = { data: {} }
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <h1>User Inputs</h1>
+      </section>
+    )
+  }
+}
+
+export default UserInputs
