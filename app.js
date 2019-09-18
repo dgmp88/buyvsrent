52ad35db5541092fa9f59982a246def2930677e0
CPrich905 <charlie.prichard@gmail.com>
2019-09-18 15:43:46 +0100

    basic maths working

--- app.js
+++ app.js
@@ -5,5 +5,21 @@ function calcSave() {
   let total = savings += result
   savInt1yr.innerHTML = result
   total1yr.innerHTML = total
+}
 
+function calcRent() {
+  let rent = +document.getElementById("rent").value
+  let bills = +document.getElementById("bills").value
+  let rbMonth = rent += bills
+  let resultRent = rbMonth * 12
+  rentbills1yr.innerHTML = resultRent
+}
+
+function calcMort() {
+  let huc = +document.getElementById("hup").value
+  let mortPay = +document.getElementById("mpay").value
+  let bills = +document.getElementById("bills").value
+  let mtMonth = mortPay += bills
+  let resultMort = mtMonth * 12
+  mortbills1yr.innerHTML = resultMort += huc
 }
