598b4a0c69aaf57d2e6b644e8a039bc9ee216562
CPrich905 <charlie.prichard@gmail.com>
2019-09-18 12:17:44 +0100

    maths on savings

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
-
 function calcSave() {
-  let savings = document.getElementById("savings").value
-  let interest = document.getElementById("savInt").value
+  let savings = +document.getElementById("savings").value
+  let interest = +document.getElementById("savInt").value
   let result = savings * interest
-  console.log('interest is', interest)
-  console.log('deposit is', savings)
-  console.log('result is', result)
+  let total = savings += result
+  savInt1yr.innerHTML = result
+  total1yr.innerHTML = total
+
 }
